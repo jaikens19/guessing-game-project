8c6a3d5860f363014f8c074bc0c3b354ab66573a
James Aikens <jaikens@jamess-mbp.attlocal.net>
2020-11-22 14:19:40 -0600

    updated guessing game to have a default of 10 trys

---
+++ guessing-game.js
@@ -0,0 +1,60 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let secretNumber = 0
+let numAttempts = 10
+
+const askRange = () => {
+    rl.question('Enter a min number: ', (answerMin) => {
+        rl.question('Enter a max number: ', (answerMax) => {
+            console.log('**************************')
+                console.log(`I'm thinking of a number between ${answerMin} and ${answerMax}`)
+                    console.log(`You have: ${numAttempts} attempts total`)
+                        console.log('**************************')
+                            randomInRange(Number(answerMin), Number(answerMax))
+                                askGuess()           
+        });
+    });
+}
+askRange()
+
+const randomInRange = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return secretNumber = Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+const askGuess = () => {
+    rl.question('Enter a guess: ', (answer) => {
+        if(checkGuess(Number(answer))){
+            console.log('You Win!')
+            rl.close()
+        } else if(numAttempts === 1){
+            console.log('You lose, try again?')
+            rl.close()
+        } else {
+            numAttempts--
+            console.log(`You have: ${numAttempts} attempts remaining`)
+            console.log('**************************')
+            askGuess()
+        } 
+    });
+}
+
+const checkGuess = (number) => {
+    if(number > secretNumber){
+        console.log('Answer too high, try again.')
+        return false
+    } else if(number < secretNumber){
+        console.log('Answer too low, try again.')
+        return false
+    } else if(number === secretNumber) {
+        console.log('Correct!')
+        return true
+    }
+}
+
